8db18fe83b377b146d4425213bb6451ea9219ded
r.ouchi <smairyu1203@gmail.com>
2018-10-07 00:13:53 +0900

    環境整備

--- asset/js/main.js
+++ asset/js/main.js
@@ -6,7 +6,7 @@ const APP = {
 
   // サイドバーを出す関数
   openSideBar: function () {
-    var self = APP,
+    let self = APP,
       target = document.getElementsByClassName('sideBar'),
       addStyle = target[0].style
     target[0].classList.add('active')
@@ -15,7 +15,7 @@ const APP = {
 
   // サイドバーをしまう関数
   closeSideBar: function () {
-    var self = APP,
+    let self = APP,
       target = document.getElementsByClassName('sideBar'),
       addStyle = target[0].style
     target[0].classList.remove('active')
@@ -24,7 +24,7 @@ const APP = {
 
   // サイドバーの状態によってイベントをハンドルする関数
   switchSideBar: function (e) {
-    var self = APP,
+    let self = APP,
       checkContains = document.getElementsByClassName('sideBar')[0].classList.contains('active')
     if (checkContains) {
       self.closeSideBar()
@@ -33,18 +33,53 @@ const APP = {
     }
   },
 
-  // DOM作成するやつ
-  volumeFieldDisplay: function () {
-    var self = APP,
-      checkValiation = document.getElementsByName('valiation'),
-      volumeField = document.getElementsByClassName('volume_field');
+  /**
+   * domCreater
+   * DOM作成するやつ
+   * @param {string} domName
+   * @param {JSON} attributes
+   */
+  domCreater: function (domName, attributes) {
+    let i = 0,
+      dom = null,
+      attributeKeys = Object.keys(attributes),
+      attributeValues = Object.values(attributes)
 
-    if (checkValiation.value === 'game') {
-      volumeField[0].style.display = 'block'
-    } else {
-      volumeField[0].style.display = 'none'
+    dom = document.createElement(domName)
+    
+    for (; i < attributeKeys.length; i++) {
+      if (attributeKeys[i] === 'text') {
+        text = document.createTextNode(attributeValues[i])
+        dom.appendChild(text)
+      } else {
+        dom.setAttribute(attributeKeys[i], attributeValues[i])
+      }
+    }
+    return dom
+  },
+
+  // 巻数のインプットを表示するやつ
+  volumeFieldDisplay: function () {
+    let self = APP,
+      checkValiation = document.getElementsByName('valiation'),
+      volumeField = document.getElementsByClassName('volume_field'),
+      inputVolume = self.domCreater('input', {
+        'type': 'number',
+        'class': 'volume',
+        'name': 'volume',
+        'id': 'volume'
+      }),
+      labelVolume = self.domCreater('label', {
+        'for': 'volume',
+        'text': '巻数：'
+      })
+
+    if (checkValiation[2].checked === true) {
+      volumeField[0].appendChild(labelVolume)
+      volumeField[0].appendChild(inputVolume)
     }
   }
+  
 }
-document.getElementById('sideBarSwitch').addEventListener('click', APP.switchSideBar);
-document.getElementById('game').addEventListener('change', APP.volumeFieldDisplay)
+document.getElementsByName('valiation')[2].addEventListener('change', APP.volumeFieldDisplay)
+document.getElementById('sideBarSwitch').addEventListener('click', APP.switchSideBar)
